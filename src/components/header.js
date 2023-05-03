be68394d34e5ccd04e47c7071384907484ff920d
minjingwei <jmin633@aucklanduni.ac.nz>
2023-05-04 08:54:36 +1200

    Push existing project to GitLab

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+//import '../styles/globals.css'
 
 export default function Header() {
   return (
-    <div>Header</div>
+        <div className='flex justify-between flex-row'>
+            {/* left */}
+            <div className='w-3/4 h-20'>
+                <Image src="/favicon.ico" width={24} height={24}/>
+            </div>
+            {/* <div class="flex "> */}
+                <div className="basis-1/4">01</div>
+                <div className="basis-1/4">02</div>
+                <div className="basis-1/2">03</div>
+            {/* </sdiv> */}
+
+            {/* middle */}
+
+            {/* right */}
+        </div>
   )
 }
