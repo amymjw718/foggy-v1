b0aa77c92682dc9df5590b86b34218a40b3ec235
minjingwei <jmin633@aucklanduni.ac.nz>
2023-05-24 16:50:47 +1200

    search bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
 //import '../styles/globals.css'
 
 export default function Header() {
@@ -9,12 +11,20 @@ export default function Header() {
             <div className='cursor-pointer h-24 w-24 relative'>
                 <Image src="/iconFoggy.png" layout='fill' className='object-contain'/>
             </div>
-            
-            <h1>Right</h1>
 
             {/* middle */}
+            <div className='relative'>
+                <div className='absolute top-1.5 left-2'>
+                    <MagnifyingGlassIcon className="h-6 w-6 m1-0" />
+                </div>
+                <input type='text' placeholder='Search' className='bg-gray-50 px-10 border-gray-600 text-sm focus:border-black rounded-md' />
+            </div>
 
             {/* right */}
+
+            <h1>Right</h1>
+
+            
         </div>
   )
 }
