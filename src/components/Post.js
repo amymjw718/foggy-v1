212b3ceda1a29d4a94a1b6e137cc7df39635354c
minjingwei <jmin633@aucklanduni.ac.nz>
2023-07-20 13:08:39 +1200

    get comments from database and show them on screen

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState,  } from 'react';
 import { EllipsisHorizontalIcon, ChatBubbleBottomCenterIcon, HeartIcon, BookmarkIcon, FaceSmileIcon } from '@heroicons/react/24/outline';
 import { useSession } from 'next-auth/react';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore';
 import { db } from '../../firebase';
+import Moment from 'react-moment';
+
 
 export default function Post({id,username,userImg,img,caption}) {
   const {data:session} = useSession();
   const [comment, setComment] = useState("");
+  const [comments, setComments] = useState([]);
+  useEffect(()=>{
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts", id, "comments"), orderBy("timestamp", "desc")
+    ),(snapshot) => {
+      setComments(snapshot.docs)
+    })
+
+  },[db, id])
 
   async function sendComment(event){
     event.preventDefault();
@@ -50,6 +61,18 @@ export default function Post({id,username,userImg,img,caption}) {
           <span className='font-bold mr-2'>{username}</span>
           {caption}
         </p>
+        {comments.length > 0 && (
+          <div className='mx-10 max-h-24 overflow-y-scroll'>
+            {comments.map((c)=>(
+              <div className='flex items-center space-x-2 mb-2'>
+                <img className='h-7 rounded-full object-cover' src={c.data().userImage} alt='userImage' />
+                <p className='font-semibold'>{c.data().username}</p>
+                <p className='flex-1 truncate'>{c.data().comment}</p>
+                <Moment fromNow>{c.data().timestamp?.toDate()}</Moment>
+              </div>
+            ))}
+          </div>
+        )}
 
         {/* post comment and input box */}
         {session && (<form className='flex items-center p-4'>
