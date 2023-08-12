a1405cd74230c913e28c8d254ab42139e2218fce
amymjw718 <80569622+amymjw718@users.noreply.github.com>
2023-08-13 10:37:58 +1200

    Update Suggestions.js

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -21,13 +21,13 @@ export default function Suggestions() {
     },[])
 
   return (
-    <div key={s.id} className="mt-4 ml-5">
+    <div className="mt-4 ml-5">
         <div className="flex justify-between mb-5 text-sm">
             <h3 className="font-bold text-gray-400">Suggestions for user</h3>
             <button className="text-gray-600 font-semibold">See all</button>
         </div>
         {Suggestions.map(s => (
-            <div className='flex items-center justify-between mt-3'>
+            <div key={s.id} className='flex items-center justify-between mt-3'>
                 <img className='h-10 rounded-full border p-[2px]' src={s.img}></img>
                 <div className='flex-1 ml-4'>
                     <h2 className='font-semibold text-sm'>{s.userName}</h2>
